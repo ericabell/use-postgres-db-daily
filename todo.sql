a3c2f6a8fe731857f43c8a4019cf705d3e959cb7
Eric Abell <ericabell@users.noreply.github.com>
2017-08-28 14:28:37 -0500

    Cleaned up with comments

--- todo.sql
+++ todo.sql
@@ -1,3 +1,7 @@
+/* drop the table if it's already there */
+DROP TABLE IF EXISTS todos;
+
+/* create the todo table per specifications */
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
@@ -7,27 +11,33 @@ CREATE TABLE todos (
   completed_at TIMESTAMP
 );
 
+/* insert 5 new todos */
 INSERT INTO todos (title, details, priority, created_at, completed_at)
-  VALUES('todo1', 'this is todo details', 2, date('2017-8-28'), NULL);
-
-INSERT INTO todos (title, details, priority, created_at, completed_at)
-  VALUES('todo1', 'this is todo details', 2, date('2017-8-28'), NULL);
+  VALUES('todo1', 'this is todo details', 1, date('2017-8-28'), NULL);
 
 INSERT INTO todos (title, details, priority, created_at, completed_at)
-  VALUES('todo1', 'this is todo details', 2, date('2017-8-28'), date('2017-8-29'));
+  VALUES('todo2', 'this is todo details', 2, date('2017-8-28'), NULL);
 
 INSERT INTO todos (title, details, priority, created_at, completed_at)
-  VALUES('todo1', 'this is todo details', 2, date('2017-8-28'), NULL);
+  VALUES('completed', 'this is todo details', 1, date('2017-8-28'), date('2017-8-29'));
 
 INSERT INTO todos (title, details, priority, created_at, completed_at)
-  VALUES('incomplete', 'this is todo details', 2, date('2017-8-28'), NULL);
+  VALUES('todo4', 'this is todo details', 2, date('2017-8-28'), NULL);
 
-SELECT * from todos WHERE completed IS NULL;
+INSERT INTO todos (title, details, priority, created_at, completed_at)
+  VALUES('incomplete', 'this is todo details', 1, date('2017-8-28'), NULL);
 
+/* get all the todos that are not completed */
+SELECT * from todos WHERE completed_at IS NULL;
+
+/* select all the todos that have priority greater than 1 */
 SELECT * from todos WHERE priority > 1;
 
+/* update the todo with title='incomplete' to be completed */
 UPDATE todos SET completed_at = date('2017-8-30') WHERE id = (SELECT id FROM todos WHERE title='incomplete');
 
+/* show all the todos to verify the update */
 SELECT * from todos;
 
+/* delete all the todos */
 DELETE FROM todos;
